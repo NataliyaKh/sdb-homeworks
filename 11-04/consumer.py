e05aa77a35b92e9db5d61eb46a7ac28f68d03fe8
Nataliya_Khanova <nataliya.khanum@gmail.com>
2024-12-02 01:47:27 +0300

    rabbit

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
-import pika
+import pika, sys, os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host = '192.168.2.137',
+    port = 5672,
+    credentials=pika.PlainCredentials(
+        username='admin',
+        password='admin'
+)))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
--- 11-04/producer.py
+++ 11-04/producer.py
@@ -2,8 +2,16 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='192.168.123.13',
+    port=5672, 
+    credentials=pika.PlainCredentials(
+        username='admin',
+        password='admin'
+)))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
+
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
+
 connection.close()
